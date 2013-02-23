b94ee8256c6a2e7391fee7a32d404cecd9dc98cd
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-02-23 18:37:26 -0500

    Changing the project link

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * @author           Pierre-Henry Soria <ph7software@gmail.com>
  * @copyright        (c) 2013, Pierre-Henry Soria. All Rights Reserved.
- * @link             http://github.com/pH-7/PHP-URL Shortener
+ * @link             http://github.com/pH-7/Slim-URL-Shortener
  * @license          GNU General Public License <http://www.gnu.org/licenses/gpl.html>
  */
 
